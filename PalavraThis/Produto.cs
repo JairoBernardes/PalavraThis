bfff976a0d74252cf4629bd261ea0c898713574a
JairoBernardes <jairo.b.ferreira@gmail.com>
2024-02-25 01:01:23 -0300

    Add reposit

---
+++ PalavraThis/Produto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalavraThis {
+    class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto() {
+           // Quantidade = 10;
+        }
+             
+        //palavra This serve também para diferenciar atributos de variáveis locais (Java)
+
+        public Produto(string nome, double preco) : this() { //usando This para passar o construtor padrao
+            Nome = nome;
+            Preco = preco;
+           // Quantidade = 5; //caso não declarar ou colocar nada aqui fica zero
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) { //usando This para passar o construtor de cima
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+
+        public override string ToString() {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ PalavraThis/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using PalavraThis;
+using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace PalavraThis {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto();
+
+            Produto p3 = new Produto { //sintaxe alternativa
+            //    Nome = "TV",
+            //    Preco = 500.00,
+            //    Quantidade = 20
+            };
+
+            Console.WriteLine("Dados do produto: " + p3);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p3.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p3);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido ao estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p3.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p3);
+
+        }
+    }
+}
